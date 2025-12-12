bac0b3c0da76dd2381e411ad9b30f5e2406dc07b
lucspinto <lucspinto@outlook.com>
2025-12-12 16:55:09 -0300

    14 - Bear Walk Forward, Idle and Walk Back

--- Assets/Bear Example/Scripts/Bear.cs
+++ Assets/Bear Example/Scripts/Bear.cs
@@ -5,8 +5,46 @@ public class Bear : MonoBehaviour
     [SerializeField] Animator _animator;
     [SerializeField] Vector2 _input;
 
+    bool _isTransitioning = false;
+    float _transitionTimer = 0f;
+    float _transitionTime = 0.1f;
+    string _currentAnimation = "Idle";
+
     void Update()
     {
         _input = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        if (_isTransitioning)
+        {
+            _transitionTimer += Time.deltaTime;
+            if (_transitionTimer > 0.1f)
+            {
+                _transitionTimer = 0f;
+                _isTransitioning = false;
+            }
+        }
+        else
+        {
+            if (_input.y > 0 && _currentAnimation != "Walk Forward")
+            {
+                _animator.CrossFade("Walk Forward", _transitionTime);
+                _isTransitioning = true;
+                _transitionTimer = 0;
+                _currentAnimation = "Walk Forward";
+            }
+            else if (_input.y == 0 && _currentAnimation != "Idle")
+            {
+                _animator.CrossFade("Idle", _transitionTime);
+                _isTransitioning = true;
+                _transitionTimer = 0;
+                _currentAnimation = "Idle";
+            }
+            else if (_input.y < 0 && _currentAnimation != "Walk Back")
+            {
+                _animator.CrossFade("Walk Back", _transitionTime);
+                _isTransitioning = true;
+                _transitionTimer = 0;
+                _currentAnimation = "Walk Back";
+            }
+        }
     }
 }
