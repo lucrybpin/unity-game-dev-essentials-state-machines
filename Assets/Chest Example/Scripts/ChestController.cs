4be0b41740d1ee6d88fe8e16a7b7e76343dc15bf
lucspinto <lucspinto@outlook.com>
2025-12-16 16:47:56 -0300

    28 - Chest Example - Adding State Machine

--- Assets/Chest Example/Scripts/ChestController.cs
+++ Assets/Chest Example/Scripts/ChestController.cs
@@ -5,33 +5,35 @@ namespace StateMachines.ChestExample
     [RequireComponent(typeof(ChestView))]
     public class ChestController : MonoBehaviour
     {
+        [field: SerializeField] public StateMachine StateMachine { get; private set; }
         [SerializeField] ChestView _view;
         [SerializeField] bool _isClosed;
 
         void Awake()
         {
+            StateMachine = new StateMachine();
             _view = GetComponent<ChestView>();
             _isClosed = true;
         }
 
-        void Start()
-        {
-            _view.OnClick += OnClick;
-        }
+        // void Start()
+        // {
+        //     _view.OnClick += OnClick;
+        // }
 
-        void OnClick()
-        {
-            if (_isClosed)
-            {
-                _isClosed = false;
-                _view.Open();
-            }
-            else
-            {
-                _isClosed = true;
-                _view.Close();
-            }
-        }
+        // void OnClick()
+        // {
+        //     if (_isClosed)
+        //     {
+        //         _isClosed = false;
+        //         _view.Open();
+        //     }
+        //     else
+        //     {
+        //         _isClosed = true;
+        //         _view.Close();
+        //     }
+        // }
 
     } // End of Class
 }
