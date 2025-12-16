a8e673b3c129ff94b7771be4a6264c38ff8c41c8
lucspinto <lucspinto@outlook.com>
2025-12-16 15:28:39 -0300

    24 - State Machine - AddState

--- Assets/Core/IState.cs
+++ Assets/Core/IState.cs
@@ -2,6 +2,7 @@ namespace StateMachines
 {
     public interface IState
     {
+        public StateMachine StateMachine { get; set; }
         void Enter();
         void Execute();
         void Exit();
--- Assets/Core/StateMachine.cs
+++ Assets/Core/StateMachine.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections.Generic;
 
 namespace StateMachines
 {
+    [Serializable]
     public class StateMachine
     {
         Dictionary<string, IState> _states = new Dictionary<string, IState>();
 
-        public void AddState(string stateName, IState state) { }
+        public void AddState(string stateName, IState state)
+        {
+            _states[stateName] = state;
+            state.StateMachine = this;
+        }
 
         public void ChangeState(string stateName) { }
 
