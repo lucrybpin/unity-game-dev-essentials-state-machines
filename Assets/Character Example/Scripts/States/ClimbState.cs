58f5fb171c32c051a1e152ecf85316717605b2bd
lucspinto <lucspinto@outlook.com>
2026-01-05 13:52:03 -0300

    75 - Character - CLIMB State - Rest and Move Up

--- Assets/Character Example/Scripts/States/ClimbState.cs
+++ Assets/Character Example/Scripts/States/ClimbState.cs
@@ -19,18 +19,29 @@ namespace StateMachines.CharacterExample
             _owner.Movement.SimulatedRigidbody(false);
 
             _owner.AnimationEvents.OnEvent1 += AnimateUp;
+            _owner.AnimationEvents.OnEvent2 += AnimateDown;
             _owner.AnimationEvents.OnEvent3 += AnimateUp;
+            _owner.AnimationEvents.OnEvent4 += AnimateDown;
         }
 
         public void Execute()
         {
-            
+            if (_owner.ActionReader.MoveAction.y == 0)
+            {
+                _owner.Animator.speed = 0;
+            }
+            else
+            {
+                _owner.Animator.speed = 1;
+            }
         }
         
         public void Exit()
         {
             _owner.AnimationEvents.OnEvent1 -= AnimateUp;
+            _owner.AnimationEvents.OnEvent2 -= AnimateDown;
             _owner.AnimationEvents.OnEvent3 -= AnimateUp;
+            _owner.AnimationEvents.OnEvent4 -= AnimateDown;
         }
 
         void AnimateUp()
@@ -38,6 +49,14 @@ namespace StateMachines.CharacterExample
             if (_owner.ActionReader.MoveAction.y > 0)
             {
                 _owner.transform.position += 0.5f * Vector3.up;
+            }
+        }
+
+        void AnimateDown()
+        {
+            if (_owner.ActionReader.MoveAction.y < 0)
+            {
+                _owner.transform.position -= 0.5f * Vector3.up;
             }
         }
     }
